ef5326430f1db1c068aa467a17a80654d9f2fc8b
Vishwajeet Singh Parihar <svishwajeet750@gmail.com>
2026-06-10 17:28:07 +0000

    tried google login

--- src/index.ts
+++ src/index.ts
@@ -64,9 +64,82 @@ const fun2 = async () => {
   await browser.close();
 };
 
+const fun3 = async () => {
+  const browser = await chromium.launch({ headless: false });
+  const context = await browser.newContext();
+
+  const page = await context.newPage();
+
+  await page.goto("https://codeforces.com/enter?back=%2F");
+  //
+  await page.locator("#handleOrEmail").fill("ninjaHattori1");
+  await page.locator("#password").fill("Sahil@Minati");
+  await page.locator("input.submit").click();
+
+  await page.waitForURL("*/codeforces");
+
+  await context.storageState({ path: "./src/auth.json" });
+
+  await browser.close();
+};
+
+const fun4 = async () => {
+  //
+  const browser = await chromium.launch({ headless: false });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  await page.goto("https://google.com");
+  let signInButton = page.locator(`a[aria-label="Sign in"]`);
+
+  await signInButton.click();
+  // const newPage = await pagePromise;
+
+  // console.log("newpage url ", newPage.url());
+  //
+  await page.locator("input#identifierId").fill("sahillovegames@gmail.com");
+  await page.locator("button").first().click();
+
+  await page.waitForTimeout(1000);
+
+  await page.locator(`input[type="email"]`).first().fill("8860283753");
+  await page.locator("button").first().click();
+
+  await page.locator(`input[name="firstName"]`).fill("20bcs053_Vishwajeet");
+  await page.locator("button").first().click();
+};
+
+const fun5 = async () => {
+  //
+  const browser = await chromium.launch({ headless: false });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  await page.goto("https://google.com");
+  let signInButton = page.locator(`a[aria-label="Sign in"]`);
+
+  await signInButton.click();
+  // const newPage = await pagePromise;
+
+  // console.log("newpage url ", newPage.url());
+  //
+  await page.locator("input#identifierId").fill("sahillovegames@gmail.com");
+  await page.locator("button").first().click();
+
+  await page.waitForTimeout(1000);
+
+  await page.locator(`input[type="email"]`).first().fill("8860283753");
+  await page.locator("button").first().click();
+
+  await page.locator(`input[name="firstName"]`).fill("20bcs053_Vishwajeet");
+  await page.locator("button").first().click();
+};
+
 const main = async () => {
-  await fun1();
+  // await fun1();
   // await fun2();
+  // await fun3();
+  await fun4();
 };
 
 main();
